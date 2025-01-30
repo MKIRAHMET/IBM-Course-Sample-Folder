96045b46e07bbd614ec23a243deb5344028c2f25
Muhammed <muhammed@192.168.2.16>
2025-01-30 21:49:20 +0200

    calculate area

--- dietary_services.js
+++ dietary_services.js
@@ -1,17 +1,18 @@
 function role () {
-let role = prompt('role');
-   let message = "";
+    let role = prompt('role');
+    let message = "";
     if (role === 'Employee') {
         message = 'You can access the Dietary Services';
-} else if (role === 'Enrolled Member') {
-    message = 'You can access the Dietary Services and the Premium Services';
-} else if (role === 'Subscriber') {
-    message = 'You have partial access to the Dietary Services';
-} else if (role === 'Non-Subscriber') {
-    message = 'You do not have access to the Dietary Services, first you have to enroll or at least subscribe.';
-} else {
-    message = 'Invalid role';
+    } else if (role === 'Enrolled Member') {
+        message = 'You can access the Dietary Services and the Premium Services';
+    } else if (role === 'Subscriber') {
+        message = 'You have partial access to the Dietary Services';
+    } else if (role === 'Non-Subscriber') {
+        message = 'You do not have access to the Dietary Services, first you have to enroll or at least subscribe.';
+    } else {
+        message = 'Invalid role';
 }
 return message;
 }
+
 document.getElementById('message').innerHTML = role();
