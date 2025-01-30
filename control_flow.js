3f7a353a35fba6fb002c639f983d976763a91a8e
Muhammed <muhammed@192.168.2.16>
2025-01-30 21:11:03 +0200

    control flow

---
+++ control_flow.js
@@ -0,0 +1,45 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === 'admin') {
+    accessLevel = "Full Access granted";
+} else if (userRole === 'manager') {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access level: ", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole === 'admin') {
+        userMessage = "Welcome, admin";
+    } else {
+        userMessage = "Hello, user";
+    }
+} else {
+    userMessage = "Please log in";
+}
+console.log("User Message:", userMessage);
+
+let userType = 'subscriber';
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("User Category:", userCategory);
