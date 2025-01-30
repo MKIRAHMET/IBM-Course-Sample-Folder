3f900453e210768b9e2de6d3c25f29a4e0ee3817
Muhammed <muhammed@192.168.2.16>
2025-01-30 21:22:49 +0200

    Dietary Services

---
+++ dietary_services.js
@@ -0,0 +1,14 @@
+let role = prompt('role');
+   let message = "";
+    if (role === 'Employee') {
+        message = 'You can access the Dietary Services';
+} else if (role === 'Enrolled Member') {
+    message = 'You can access the Dietary Services and the Premium Services';
+} else if (role === 'Subscriber') {
+    message = 'You have partial access to the Dietary Services';
+} else if (role === 'Non-Subscriber') {
+    message = 'You do not have access to the Dietary Services, first you have to enroll or at least subscribe.';
+} else {
+    message = 'Invalid role';
+}
+document.getElementById('message').innerHTML = message;
